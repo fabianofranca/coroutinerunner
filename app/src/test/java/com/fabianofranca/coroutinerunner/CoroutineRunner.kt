63baf500c56890e9a9ce3c88c693c3f06e556e3b
Fabiano França <email@fabianofranca.com>
2018-05-29 01:05:34 +0000

    Create validation for coroutine test methods

--- app/src/test/java/com/fabianofranca/coroutinerunner/CoroutineRunner.kt
+++ app/src/test/java/com/fabianofranca/coroutinerunner/CoroutineRunner.kt
@@ -8,35 +8,56 @@ import java.lang.reflect.Method
 
 open class CoroutineRunner(testClass: Class<*>) : BlockJUnit4ClassRunner(testClass) {
 
-    val async = AsyncTestRunner()
+    private lateinit var async: AsyncTestRunner
+    private lateinit var coroutineMethods: List<CoroutineFrameworkMethod>
+
+    protected fun getCoroutineChildren(): List<CoroutineFrameworkMethod> {
+        if (!::coroutineMethods.isInitialized) {
+            val methods = mutableListOf<CoroutineFrameworkMethod>()
+
+            if (!::async.isInitialized) {
+                async = AsyncTestRunner()
+            }
+
+            val asyncMethod = async::class.java.getMethod(AsyncTestRunner::run.name)
+
+            testClass.getAnnotatedMethods(CoroutineTest::class.java).forEach {
+                methods.add(CoroutineFrameworkMethod(it.method.name, asyncMethod))
+            }
+
+            coroutineMethods = methods
+        }
+
+        return coroutineMethods
+    }
 
     override fun getChildren(): MutableList<FrameworkMethod> {
         val children = mutableListOf<FrameworkMethod>()
         children.addAll(super.getChildren())
-
-        val coroutineMethods = mutableListOf<FrameworkMethod>()
-
-        val method = async::class.java.getMethod(AsyncTestRunner::run.name)
-
-        getCoroutineChildren().forEach {
-            coroutineMethods.add(CoroutineFrameworkMethod(it.method.name, method))
-        }
-
-        children.addAll(coroutineMethods)
+        children.addAll(getCoroutineChildren())
 
         return children
     }
 
-    protected fun getCoroutineChildren(): List<FrameworkMethod> {
-        return testClass.getAnnotatedMethods(CoroutineTest::class.java)
-    }
-
     override fun methodInvoker(method: FrameworkMethod?, test: Any?): Statement {
-
         return if (method is CoroutineFrameworkMethod) {
             InvokeMethod(method, async)
         } else {
             super.methodInvoker(method, test)
+        }
+    }
+
+    override fun validateTestMethods(errors: MutableList<Throwable>) {
+        super.validateTestMethods(errors)
+        validatePublicVoidNoArgCoroutineMethods(false, errors)
+    }
+
+    protected fun validatePublicVoidNoArgCoroutineMethods(
+        isStatic: Boolean,
+        errors: MutableList<Throwable>
+    ) {
+        for (method in getCoroutineChildren()) {
+            method.validatePublicVoidNoArg(isStatic, errors)
         }
     }
 
@@ -53,6 +74,16 @@ class CoroutineFrameworkMethod(private val name: String, method: Method) :
 
     override fun getName(): String {
         return name
+    }
+
+    override fun validatePublicVoidNoArg(isStatic: Boolean, errors: MutableList<Throwable>?) {
+        if (isStatic() != isStatic) {
+            val state = if (isStatic) "should" else "should not"
+            errors?.add(Exception("Method " + method.name + "() " + state + " be static"))
+        }
+        if (!isPublic) {
+            errors?.add(Exception("Method " + method.name + "() should be public"))
+        }
     }
 }
 
